74d45f7aee7aa1280cd2903281d4c1fc0f6787a4
Simen <simen_sor91@hotmail.com>
2022-04-03 15:48:34 +0200

    Update WebpageExample.ino
    Subscribe to the renamed events and restructure code to be more generic.

--- WebpageExample/WebpageExample.ino
+++ WebpageExample/WebpageExample.ino
@@ -1,43 +1,28 @@
 /*******************************************************************
-    A sketch for controlling a toy car with using a web page 
-    hosted on a ESP8266
+    An example sketch for interacting with an ESP32 using a 
+    web interface
     
-    Main Hardware:
-    - NodeMCU Development Board cp2102 (Look for the one with the square chip beside the USB port)
-    - NodeMCU Motor Shield (L2932)
-    
-    Written by Brian Lough
+    Based on an RC Car example written by Brian Lough
     https://www.youtube.com/channel/UCezJOfu7OtqGzd5xrP3q6WA
+    https://github.com/witnessmenow/simple-wifi-controlled-rc-car
  *******************************************************************/
 
-#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
-// These are the pins used to control the motor shield
+// These pins are used to indicate button press. They can be connected to LEDs for visualization of working code.
+#define PIN1 14
+#define PIN2 27
+#define PIN3 26
+#define PIN4 25
 
-#define DRIVE_MOTOR_POWER D2 // Motor B
-#define DRIVE_MOTOR_DIRECTION D4
-
-#define STEER_MOTOR_POWER D1 // Motor A
-#define STEER_MOTOR_DIRECTION D3
-
-// drivePower sets how fast the car goes
-// Can be set between 0 and 1023 (although car problaly wont move if values are too low)
-int drivePower = 1023;
-
-// driveDirection sets what direction the car drives
-// If the car is moving backwards when you press the forward button, change this to LOW
-uint8_t driveDirection = HIGH;
-
-// steeringPower sets how fast the car turns
-// Can be set between 0 and 1023 (again, car probably won't steer if the value is too low)
-int steeringPower = 1023;
-
-// steerDirection sets what direction the car steers
-// If the car is steering right when you press the left button, change this to LOW
-uint8_t steerDirection = HIGH;
+volatile bool BUTTONDOWNFLAG1 = false;
+volatile bool BUTTONDOWNFLAG2 = false;
+volatile bool BUTTONDOWNFLAG3 = false;
+volatile bool BUTTONDOWNFLAG4 = false;
+volatile bool BUTTONRELEASEFLAG_COMMON = false;
 
 // ----------------
 // Set your WiFi SSID and Password here
@@ -45,10 +30,10 @@ uint8_t steerDirection = HIGH;
 const char* ssid = "ssid";
 const char* password = "password";
 
-ESP8266WebServer server(80);
+WebServer server(80);
 
 const char *webpage = 
-#include "motorPage.h"
+#include "webPage.h"
 ;
 
 void handleRoot() {
@@ -71,12 +56,44 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
 }
 
+void buttondown_handler1(){
+  BUTTONDOWNFLAG1 = false;
+  digitalWrite(PIN1, HIGH);
+}
+
+void buttondown_handler2(){
+  BUTTONDOWNFLAG2 = false;
+  digitalWrite(PIN2, HIGH);
+}
+
+void buttondown_handler3(){
+  BUTTONDOWNFLAG3 = false;
+  digitalWrite(PIN3, HIGH);
+}
+
+void buttondown_handler4(){
+  BUTTONDOWNFLAG4 = false;
+  digitalWrite(PIN4, HIGH);
+}
+
+void buttonrelease_handler_common(){
+  digitalWrite(PIN1, LOW);
+  digitalWrite(PIN2, LOW);
+  digitalWrite(PIN3, LOW);
+  digitalWrite(PIN4, LOW);
+  BUTTONRELEASEFLAG_COMMON = false;
+}
+
 void setup(void){
 
-  pinMode(DRIVE_MOTOR_POWER, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  pinMode(DRIVE_MOTOR_DIRECTION, OUTPUT);
-  pinMode(STEER_MOTOR_POWER, OUTPUT);
-  pinMode(STEER_MOTOR_DIRECTION, OUTPUT);
+   pinMode(PIN1, OUTPUT);    
+   pinMode(PIN2, OUTPUT);
+   pinMode(PIN3, OUTPUT);
+   pinMode(PIN4, OUTPUT);
+   digitalWrite(PIN1, LOW);
+   digitalWrite(PIN2, LOW);
+   digitalWrite(PIN3, LOW);
+   digitalWrite(PIN4, LOW);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -97,50 +114,40 @@ void setup(void){
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("WifiCar")) {
+  if (MDNS.begin("ButtonExample")) {
     Serial.println("MDNS Responder Started");
   }
 
   server.on("/", handleRoot);
 
-  server.on("/forward", [](){
-    Serial.println("forward");
-    analogWrite(DRIVE_MOTOR_POWER, drivePower);
-    digitalWrite(DRIVE_MOTOR_DIRECTION, driveDirection);
-    server.send(200, "text/plain", "forward");
+  server.on("/up", [](){
+    Serial.println("up - buttondown1");
+    BUTTONDOWNFLAG1 = true;
+    server.send(200, "text/plain", "up");
   });
 
-  server.on("/driveStop", [](){
-    Serial.println("driveStop");
-    analogWrite(DRIVE_MOTOR_POWER, 0);
-    server.send(200, "text/plain", "driveStop");
-  });
-
-  server.on("/back", [](){
-    Serial.println("back");
-    analogWrite(DRIVE_MOTOR_POWER, drivePower);
-    digitalWrite(DRIVE_MOTOR_DIRECTION, !driveDirection);
-    server.send(200, "text/plain", "back");
+  server.on("/down", [](){
+    Serial.println("down - buttondown2");
+    BUTTONDOWNFLAG2 = true;
+    server.send(200, "text/plain", "down");
   });
 
   server.on("/right", [](){
-    Serial.println("right");
-    analogWrite(STEER_MOTOR_POWER, steeringPower);
-    digitalWrite(STEER_MOTOR_DIRECTION, steerDirection);
+    Serial.println("right - buttondown3");
+    BUTTONDOWNFLAG3 = true;
     server.send(200, "text/plain", "right");
   });
 
   server.on("/left", [](){
-    Serial.println("left");
-    analogWrite(STEER_MOTOR_POWER, steeringPower);
-    digitalWrite(STEER_MOTOR_DIRECTION, !steerDirection);
+    Serial.println("left - buttondown4");
+    BUTTONDOWNFLAG4 = true;
     server.send(200, "text/plain", "left");
   });
 
-  server.on("/steerStop", [](){
-    Serial.println("steerStop");
-    analogWrite(STEER_MOTOR_POWER, 0);
-    server.send(200, "text/plain", "steerStop");
+  server.on("/buttonrelease", [](){
+    Serial.println("ButtonRelease");
+    BUTTONRELEASEFLAG_COMMON = true;
+    server.send(200, "text/plain", "buttonrelease");
   });
 
   server.onNotFound(handleNotFound);
@@ -151,4 +158,15 @@ void setup(void){
 
 void loop(void){
   server.handleClient();
+  if(BUTTONDOWNFLAG1){
+    buttondown_handler1();
+  }else if(BUTTONDOWNFLAG2){
+    buttondown_handler2();
+  }else if(BUTTONDOWNFLAG3){
+    buttondown_handler3();
+  }else if(BUTTONDOWNFLAG4){
+    buttondown_handler4();
+  }else if(BUTTONRELEASEFLAG_COMMON){
+    buttonrelease_handler_common();
+  }
 }
